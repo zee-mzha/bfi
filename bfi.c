11e57403f1dcebb676f60fa7eb400bbe87446f5e
zee220 <mustafa@zee.technology>
2019-03-03 02:04:33 -0700

    more stuff added, renamed functions, better erro messages and no longer holding onto the file pointer

--- bfi.c
+++ bfi.c
@@ -8,12 +8,14 @@
 #define BFI_FILE_OPEN 1
 #define BFI_FILE_READ 2
 #define BFI_MEM 3
+#define BFI_OUT_OF_BOUNDS 4
+//the brainfuck program has an error in it
+#define BFI_PROG_ERROR 5
 
 typedef struct bfFile{
-	FILE *file;
 	char *program;
-	long index;
-	long progIndex;
+	long memoryIndex;
+	long programIndex;
 	long programSize;
 	char memory[BFI_MEMSIZE];
 } bfFile;
@@ -26,62 +28,58 @@ BFI_FILE_OPEN
 BFI_FILE_READ
 BFI_MEM
 */
-int bfFileLoad(const char *path, bfFile *res);
+int bfLoad(const char *path, bfFile *res);
+
+void bfDestroy(bfFile *f);
 
 /*
-clean up
+parses and executes an operator at program[prodIndex]
 */
-void bfFileDestroy(bfFile *f);
+int bfInterpretOperator(bfFile *f);
 
-void bfParseOperator(bfFile *f);
+int bfInterpret(bfFile *file);
 
-/*
-parses and executes an operator
-*/
-void bfFileParse(bfFile *file);
-
-/*
-parses loops, no touchy
-*/
-void bfParseLoop(bfFile *file);
+void bfLoop(bfFile *file);
 
 int main(int argc, char **argv){
 	if(argc == 1){
-		printf("proper usage: %s file.bf file2.bf file3.bf ...\n", argv[0]);
+		printf("proper usage: \n\tfor a single file: %s file.bf \n\tfor multiple files: %s file1.bf file2.bf file3.bf ...\n", argv[0], argv[0]);
 		return 0;
 	}
 
 	for(int i = 1; i < argc; i++){
 		bfFile file;
-		int ret = bfFileLoad(argv[i], &file);
+		int ret = bfLoad(argv[i], &file);
 		if(ret != BFI_OK){
 			switch(ret){
 				case BFI_FILE_OPEN:
-					printf("failed to open file: %s\n", argv[i]);
+					fprintf(stderr, "failed to open file: %s\n", argv[i]);
 					break;
 				case BFI_FILE_READ:
-					printf("failed to read file: %s\n", argv[i]);
+					fprintf(stderr, "failed to read file: %s\n", argv[i]);
 					break;
 				case BFI_MEM:
-					printf("failed to allocate memory for file: %s\n", argv[i]);
+					fprintf(stderr, "failed to allocate memory for file: %s\n", argv[i]);
 					break;
 			}
 			continue;
 		}
 
-		bfFileParse(&file);
-		bfFileDestroy(&file);
+		if(bfInterpret(&file) != BFI_OK){
+			fprintf(stderr, "error in file: %s\n", argv[i]);
+		}
+
+		bfDestroy(&file);
 	}
 
 	return 0;
 }
 
 //load a file from a path
-int bfFileLoad(const char *path, bfFile *res){
-	res->file = NULL;
+int bfLoad(const char *path, bfFile *res){
 	res->program = NULL;
-	res->index = 0;
-	res->progIndex = 0;
+	res->memoryIndex = 0;
+	res->programIndex = 0;
 	res->programSize = 0;
 
 	FILE *f = fopen(path, "r");
@@ -105,7 +103,8 @@ int bfFileLoad(const char *path, bfFile *res){
 		return BFI_FILE_READ;
 	}
 
-	res->file = f;
+	fclose(f);
+
 	res->program = fData;
 	res->programSize = fileSize;
 	memset(res->memory, 0, BFI_MEMSIZE);
@@ -113,61 +112,81 @@ int bfFileLoad(const char *path, bfFile *res){
 	return BFI_OK;
 }
 
-void bfFileDestroy(bfFile *f){
-	fclose(f->file);
-	f->file = NULL;
-
+void bfDestroy(bfFile *f){
 	free(f->program);
 	f->program = NULL;
 }
 
-void bfParseOperator(bfFile *f){
-	switch(f->program[f->progIndex]){
+int bfInterpretOperator(bfFile *f){
+	switch(f->program[f->programIndex]){
 		case '>':
-			f->index++;
+			if(f->memoryIndex >= BFI_MEMSIZE){
+				fprintf(stderr, "program attempted to move memory pointer higher than the upper bounds of %u\n", BFI_MEMSIZE);
+				return BFI_OUT_OF_BOUNDS;
+			}
+			f->memoryIndex++;
 			break;
 		case '<':
-			f->index--;
+			if(f->memoryIndex <= 0){
+				fputs("program attempted to move memory pointer below 0\n", stderr);
+				return BFI_OUT_OF_BOUNDS;
+			}
+			f->memoryIndex--;
 			break;
 		case '+':
-			f->memory[f->index]++;
+			f->memory[f->memoryIndex]++;
 			break;
 		case '-':
-			f->memory[f->index]--;
+			f->memory[f->memoryIndex]--;
 			break;
 		case '.':
-			putchar(f->memory[f->index]);
+			putchar(f->memory[f->memoryIndex]);
 			break;
 		case ',':
-			f->memory[f->index] = getchar();
-			getchar();
+			f->memory[f->memoryIndex] = getchar();
 			break;
 		case '[':
-			bfParseLoop(f);
+			bfLoop(f);
 			break;
 	}
+
+	return BFI_OK;
 }
 
-void bfFileParse(bfFile *f){
-	for(;f->progIndex < f->programSize; f->progIndex++){
-		bfParseOperator(f);
+int bfInterpret(bfFile *f){
+	for(;f->programIndex < f->programSize; f->programIndex++){
+		if(bfInterpretOperator(f) != BFI_OK){
+			//print 8 before and 8 after the current programIndex
+			int bytesBefore = 8;
+			int bytesAfter = 8;
+			if(f->programIndex < 7){
+				bytesBefore = f->programIndex;
+			}
+			if((f->programIndex + bytesAfter) > f->programSize){
+				bytesAfter = f->programSize-f->programIndex;
+			}
+			fprintf(stderr, "error at character %d:\n\t%.*s\n", f->programIndex, bytesBefore+bytesAfter, &f->program[f->programIndex-bytesBefore]);
+			fprintf(stderr, "\t%*s\n", bytesBefore+1, "^");
+			return BFI_PROG_ERROR;
+		}
 	}
+	return BFI_OK;
 }
 
-void bfParseLoop(bfFile *f){
-	f->progIndex++;
-	long start = f->progIndex;
-	for(;f->progIndex < f->programSize; f->progIndex++){
-		if(f->program[f->progIndex] == ']'){
-			if(f->memory[f->index]){
-				f->progIndex = start;
+void bfLoop(bfFile *f){
+	f->programIndex++;
+	long start = f->programIndex;
+	for(;f->programIndex < f->programSize; f->programIndex++){
+		if(f->program[f->programIndex] == ']'){
+			if(f->memory[f->memoryIndex]){
+				f->programIndex = start;
 			}
 			else{
-				f->progIndex++;
+				f->programIndex++;
 				break;
 			}
 		}
 
-		bfParseOperator(f);
+		bfInterpretOperator(f);
 	}
 }
